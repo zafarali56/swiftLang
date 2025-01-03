a310f3ee0a3eb0143a12985dc13e2d2599cf0c09
zafar ali <zafaralyjamali722@gmail.com>
2025-01-04 00:00:36 +0500

    check point 1

---
+++ 100daysOfSwift.playground/Pages/check_Point1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
+
+
+/*Creates a constant holding any temperature in Celsius.
+Converts it to Fahrenheit by multiplying by 9, dividing by 5, then adding 32.
+Prints the result for the user, showing both the Celsius and Fahrenheit values.*/
+
+let Celcius : Int = 34
+let Fahrenheit: Int = (Celcius * 9)/5+32
+print(Fahrenheit)
+
