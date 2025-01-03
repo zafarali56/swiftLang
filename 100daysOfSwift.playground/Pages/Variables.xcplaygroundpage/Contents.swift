d1760f4c651e3d6a0d8cb1342a66e23a4f18cfa1
zafar ali <zafaralyjamali722@gmail.com>
2025-01-03 16:17:15 +0500

    Initial Commit

---
+++ 100daysOfSwift.playground/Pages/Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+
+var greeting = "Hello, playground"; let constant = "Constant in swift"
+//mutable variable
+var changable : Int = 10
+changable = 50
+print(changable)
+
+let UnChangable : Int = 30
+
+
+//immutable variable cannot be changed
+UnChangable = 50
+print(UnChangable)
+
+
